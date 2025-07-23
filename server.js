63be3c2089645dba613648059dff2893ee3f06be
unknown <michaelkifle321@gmail.com>
2025-07-23 10:07:15 +0300

    changes

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.get('/' , (req , res)=>{
 
 connectToDatabase()
 
-app.listen(8000 , ()=>{
-    console.log('server is connected on port 8000')
+app.listen(process.env.PORT , ()=>{
+    console.log('server is connected ')
 })
